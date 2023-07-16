ca4d931fce2ed1dd9392d8317dc290d286d7a713
Torben Vollbrecht <torben.vollbrecht@gmail.com>
2023-07-16 13:12:23 +0200

    Use bcrypt password encoder

--- src/main/java/de/torben/configuration/WebSecurityConfiguration.java
+++ src/main/java/de/torben/configuration/WebSecurityConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,26 +20,19 @@ public class WebSecurityConfiguration {
 
     @Bean
     public MapReactiveUserDetailsService userDetailsService() {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = User.withUsername("user").password(getPasswordEncoder().encode("user")).roles("USER").build();
 
-        UserDetails user = users
-                .username("user")
-                .password("user")
-                .roles("USER")
-                .build();
         return new MapReactiveUserDetailsService(user);
     }
 
-
+    @Bean
+    PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(
-            ServerHttpSecurity http) {
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable).formLogin(withDefaults())
-                .authorizeExchange((authorizeHttpRequests) ->
-                authorizeHttpRequests
-                        .pathMatchers("/**").hasRole("USER")
-                );
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable).formLogin(withDefaults()).authorizeExchange((authorizeHttpRequests) -> authorizeHttpRequests.pathMatchers("/**").hasRole("USER"));
         return http.build();
     }
 
